ae25264fe626eb5edfdd822a93f5c076631e64af
Mehedimid <mehedihasan25276@gmail.com>
2023-11-26 02:33:50 +0600

    header section designed

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../shared components/footer/Footer';
 
 function Layout(props) {
     return (
         <div>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 }
--- src/pages/Home/Header.jsx
+++ src/pages/Home/Header.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import Banner from './Banner';
-import Navbar from '../../shared components/navbar/Navbar';
+import React from "react";
+import Banner from "./Banner";
+import Navbar from "../../shared components/navbar/Navbar";
 
 function Header(props) {
-    return (
-        <div>
-            <Banner></Banner>
-            <Navbar></Navbar>
+  return (
+    <>
+      <Banner></Banner>
+
+      <Navbar></Navbar>
+
+      <div className="hero min-h-screen ">
+        <div className="hero-content text-white text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Shofor <span className="text-red-500">Pothe</span> LTD.</h1>
+            <p className="py-6">
+            Start your journey with us and let the wonders of Bangladesh unfold before your eyes. Explore, experience, and create memories that last a lifetime!
+            </p>
+            <button className="my-btn">Register</button>
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 }
 
 export default Header;
